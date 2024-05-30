93081f06de0a4002e3d6220e71da066c9437ec70
Abhishekkrsingh2023 <abhishekkrsingh2023kolkata@gmail.com>
2024-05-30 22:27:30 +0530

    New code added

---
+++ python_basics/basic1.py
@@ -0,0 +1,23 @@
+
+# here is the basic syntax to print any statement
+#  print(string/variable) 
+
+print("Hello_world!!") # here the print() function displays the string or variable passed in it
+
+# or
+
+print(5+3)
+
+# or
+
+x=50 # variable x with value 50
+print(x)
+
+# or
+
+name="pyhton_course" # string variable name
+print(name)
+
+# or
+# can even pass multiple variables
+print(name,"is a playlist provided by TechQuesters")
